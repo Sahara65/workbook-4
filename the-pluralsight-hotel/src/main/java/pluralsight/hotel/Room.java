4b72200b33ecfb14841a12eeb3e28f9ce3689ea4
Sahara <sahara.tm65@gmail.com>
2023-11-03 15:30:17 -0400

    Finished Room Class

--- the-pluralsight-hotel/src/main/java/pluralsight/hotel/Room.java
+++ the-pluralsight-hotel/src/main/java/pluralsight/hotel/Room.java
@@ -6,7 +6,26 @@ public class Room {
     boolean isClean;
     RoomType roomType;
 
+    public Room(int number, boolean available, boolean isClean, RoomType roomType) {
+        this.number = number;
+        this.available = available;
+        this.isClean = isClean;
+        this.roomType = roomType;
+    }
 
+    public int getNumber() {
+        return number;
+    }
 
+    public boolean isAvailable() {
+        return available;
+    }
 
+    public boolean isClean() {
+        return isClean;
+    }
+
+    public RoomType getRoomType() {
+        return roomType;
+    }
 }
